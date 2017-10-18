6461e6392897e318fb307d082b84f971942fc154
Vincenzo Della Mea @ Medical Informatics & Telemedicine Lab, UNIUD <vincenzo.dellamea@uniud.it>
2017-10-18 09:37:52 -0500

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,36 @@
+import requests
+
+token_endpoint = 'https://icdaccessmanagement.who.int/connect/token'
+client_id = '...'
+client_secret = '...'
+scope = 'icdapi_access'
+grant_type = 'client_credentials'
+
+
+# get the OAUTH2 token
+
+# set data to post
+payload = {'client_id': client_id, 
+	   	   'client_secret': client_secret, 
+           'scope': scope, 
+           'grant_type': grant_type}
+           
+# make request
+r = requests.post(token_endpoint, data=payload, verify=False).json()
+token = r['access_token']
+
+
+# access ICD API
+
+uri = 'https://id.who.int/icd/entity'
+
+# HTTP header fields to set
+headers = {'Authorization':  'Bearer '+token, 
+           'Accept': 'application/json', 
+           'Accept-Language': 'en'}
+           
+# make request           
+r = requests.get(uri, headers=headers, verify=False)
+
+# print the result
+print (r.text)			
